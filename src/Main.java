81b9076d23c936cf4a907ea70d1d6210db4bc37c
Valentin <zhe-valentin@mail.ru>
2024-08-28 01:06:24 +0700

    HW-6 string 1-3

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,16 @@ public class Main {
         System.out.println("Ф. И. О. сотрудника — " + fullName);
 
         System.out.println("Задача №2");
+        // использую метод toUpperCase()
+        // этот метод преобразует все знаки в строке в верхний регистр
+        String upperCaseName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " +
+                upperCaseName);
 
-
+        System.out.println("Задача №3");
+        // нужно заменить символ «ё» на символ «е»
+        String fullName2 = "Иванов Семён Семёнович";
+        String replacementFullName2 = fullName2.replace("ё", "e");
+        System.out.println("Данные Ф. И. О. сотрудника — " + replacementFullName2);
     }
 }
