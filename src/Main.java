6a2eb9e0111133548f2f162ea42cb28571b7dd50
Valentin <zhe-valentin@mail.ru>
2024-08-28 00:32:58 +0700

    HW-6 string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1");
+        // создать четыре строки, хронящие Ф. И. О. сотрудника
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача №2");
+
+
     }
 }
